ce77e8f581f50ec8a1d85963112f33720830c513
Oliver <oliverk120@gmail.com>
2015-01-19 11:40:52 -0500

    installed nodetime

--- config/env/development.js
+++ config/env/development.js
@@ -9,7 +9,7 @@ module.exports = {
     debug: false
   },
   app: {
-    name: 'MEAN - FullStack JS - Development'
+    name: 'Givetu - Romantic'
   },
   facebook: {
     clientID: 'DEFAULT_APP_ID',
--- config/env/production.js
+++ config/env/production.js
@@ -27,7 +27,7 @@ module.exports = {
     */
   },
   app: {
-    name: 'MEAN - A Modern Stack - Production'
+    name: 'Givetu - Romantic'
   },
   facebook: {
     clientID: 'APP_ID',
