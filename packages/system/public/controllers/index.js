1a7157ef7769795b8ad7b0868f6e62c7316ac7e1
Oliver <oliverk120@gmail.com>
2015-01-20 10:47:25 -0500

    not sure what this is

--- packages/system/public/controllers/index.js
+++ packages/system/public/controllers/index.js
@@ -13,14 +13,13 @@ $scope.options = {
   },
   price:{
     price1:{name:'<20', value:'0-20'},
-    price2:{name:'20 - 50', value:'21-50'},
-    price3:{name:'50 - 200', value:'51-200'},
-    price4:{name:'+200', value:'201-10000000'}
+    price2:{name:'20 - 100', value:'21-100'},
+    price3:{name:'+100', value:'101-10000000'}
   },
   level:{
     low:{name:'normal', value:0},
     medium:{name:'cheesy', value:1},
-    high:{name:'very cheesy', value:2},
+    //high:{name:'very cheesy', value:2},
   }
 };
 }
