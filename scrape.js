bcd1fcb463f5aa2f0aadae01ae748effb5c96621
oliverk120 <oliverk120@gmail.com>
2015-02-11 00:36:05 -0500

    added scraper

---
+++ scrape.js
@@ -0,0 +1,16 @@
+'use strict'
+
+var request = require('request'),
+cheerio = require('cheerio'),
+list = [];
+
+request('http://www.thisiswhyimbroke.com/new/', function(err, res, body){
+	if(!err && res.statusCode === 200){
+		var $ = cheerio.load(body);
+		$('div.item a img').each(function(){
+			//console.log(this.attribs.src);
+			list.push(this.attribs.src);
+		});
+	}
+	console.log(list);
+});
